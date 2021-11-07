e755231b2c080dc9ea6198e5ea69d1f43e10984a
MiraBellierr <75848@siswa.unimas.my>
2021-11-07 13:17:40 +0800

    add meme command

---
+++ src/main/java/commands/Meme/Meme.java
@@ -0,0 +1,87 @@
+package commands.Meme;
+
+import com.google.gson.Gson;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class Meme {
+
+    private String name = "Meme";
+    private String Description = "Returns a meme";
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void run(MessageReceivedEvent event, ArrayList<String> args) throws IOException, InterruptedException {
+        sendRandomMeme(event);
+    }
+
+    private void sendRandomMeme(@NotNull MessageReceivedEvent event) throws IOException, InterruptedException {
+        String[] reddits = { "memes", "dankmemes", "Memes_Of_The_Dank", "wholesomememes", "terriblefacebookmemes", "pewdiepiesubmissions" };
+        String random = reddits[(int) Math.floor(Math.random() * reddits.length)];
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(String.format("https://www.reddit.com/r/%s/hot.json", random)))
+                .build();
+
+        var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        Memes obj = new Gson().fromJson(response.body(), Memes.class);
+        DataRetrieve result = obj.data.children.get((int) Math.floor(Math.random() * obj.data.children.toArray().length)).data;
+
+        EmbedBuilder embed = new EmbedBuilder()
+                .setAuthor(String.format("By %s - ", result.author), null, result.all_awardings.toArray().length > 0 ? result.all_awardings.get((int) Math.floor(Math.random() * result.all_awardings.toArray().length)).icon_url : "")
+                .setTitle(result.title, String.format("https://www.reddit.com%s", result.permalink))
+                .setImage(result.url)
+                .setFooter(String.format("⬆️ %d | \uD83D\uDCAC %d | \uD83C\uDFC5 %d", result.ups, result.num_comments, result.total_awards_received));
+
+        event.getChannel().sendMessage(embed.build()).queue();
+    }
+}
+
+class Memes
+{
+    Data data;
+}
+
+class Data
+{
+    ArrayList<ChildrenData> children;
+}
+
+class ChildrenData {
+    DataRetrieve data;
+}
+
+
+class DataRetrieve {
+    boolean over_18;
+    boolean is_video;
+    String author;
+    ArrayList<Icon> all_awardings;
+    String title;
+    String permalink;
+    String url;
+    int ups;
+    int num_comments;
+    int total_awards_received;
+}
+
+class Icon {
+    String icon_url;
+}
