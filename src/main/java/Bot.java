e2959a13b19652ef9a4eaa1503d6d2ebd83b85fc
MiraBellierr <75848@siswa.unimas.my>
2021-11-12 14:31:51 +0800

    optimize the code

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -3,26 +3,19 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
+import utils.Config;
 import utils.Mongo;
 import utils.Prefix;
 
 import javax.security.auth.login.LoginException;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.EnumSet;
-import java.util.Properties;
 
 public class Bot {
 
     public static void main(String[] args) throws LoginException, ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException, IOException, InterruptedException {
-        Properties prop = new Properties();
-        String fileName = "./bot.config";
-
-        FileInputStream fis = new FileInputStream(fileName);
-        prop.load(fis);
-
-        JDABuilder builder = JDABuilder.createDefault(prop.getProperty("TOKEN"), EnumSet.allOf(GatewayIntent.class))
+        JDABuilder builder = JDABuilder.createDefault(new Config().getConfig().getProperty("TOKEN"), EnumSet.allOf(GatewayIntent.class))
                 .setActivity(Activity.playing(String.format("Type %shelp", new Prefix().getPrefix())))
                 .setMemberCachePolicy(MemberCachePolicy.ALL);
 
