e59d06388e9281ae53567914cb5d87933514b607
MiraBellierr <75848@siswa.unimas.my>
2021-11-14 00:06:12 +0800

    to final

--- src/main/java/utils/Mongo.java
+++ src/main/java/utils/Mongo.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 
 public class Mongo {
 
-    private MongoClient mongoClient;
+    private final MongoClient mongoClient;
 
     public Mongo() throws IOException {
         this.mongoClient = new MongoClient(new MongoClientURI(String.format("mongodb+srv://%s:%s@%s?retryWrites=true&w=majority", new Config().getConfig().getProperty("MONGO_USERNAME"), new Config().getConfig().getProperty("MONGO_PASSWORD"), new Config().getConfig().getProperty("MONGO_CONNECTION"))));
